85cfab349ba4497fb1989a13e39667092c0b2467
Fire-Dragon-DoL <francesco.belladonna@gmail.com>
2018-11-24 22:37:05 -0800

    Add MessageStore callbacks

--- lib/rill/message_store.ex
+++ lib/rill/message_store.ex
@@ -1,6 +1,31 @@
 defmodule Rill.MessageStore do
-  # @callback read(session, stream_name, opts)
-  # @callback write(session, stream_name, opts)
-  # @callback write_initial(session, stream_name, opts)
-  # @callback write_reply(session, previous_message, opts)
+  @type read_option(
+          {:position, non_neg_integer()}
+          | {:batch_size, pos_integer()}
+        )
+  @doc """
+  Returned enumerable must be a stream from the given position until the end
+  of the stream
+  """
+  @callback read(
+              session :: term(),
+              stream_name :: String.t(),
+              opts :: [read_option()]
+            ) :: Enumerable.t()
+
+  @type write_option(
+          {:expected_version, Rill.MessageStore.ExpectedVersion.t()}
+          | {:reply_stream_name, String.t() | nil}
+        )
+  @callback write(
+              session :: term(),
+              message :: struct(),
+              stream_name :: String.t(),
+              opts :: [write_option()]
+            ) :: non_neg_integer()
+  @callback write_initial(
+              session :: term(),
+              message :: struct(),
+              stream_name :: String.t()
+            ) :: non_neg_integer()
 end
