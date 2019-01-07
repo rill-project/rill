ca31d52dad47e9a6304c46cb73373264c5ec911f
Fire-Dragon-DoL <francesco.belladonna@gmail.com>
2019-01-07 18:08:22 +0100

    Add multiple events entity projection test

--- test/automated/rill/entity_projection_test.exs
+++ test/automated/rill/entity_projection_test.exs
@@ -2,12 +2,17 @@ defmodule Rill.EntityProjectionTest do
   use AsyncCase
 
   defmodule Person do
-    defstruct name: ""
+    defstruct name: "", age: 0
   end
 
   defmodule Renamed do
     use Rill, :message
     defmessage([:name])
+  end
+
+  defmodule Aged do
+    use Rill, :message
+    defmessage([:amount])
   end
 
   defmodule Projection do
@@ -16,6 +21,12 @@ defmodule Rill.EntityProjectionTest do
     @impl true
     deftranslate apply(%Renamed{} = renamed, person) do
       Map.put(person, :name, renamed.name)
+    end
+
+    @impl true
+    deftranslate apply(%Aged{} = aged, person) do
+      age = person.age + aged.amount
+      Map.put(person, :age, age)
     end
   end
 
@@ -29,6 +40,18 @@ defmodule Rill.EntityProjectionTest do
       new_person = EntityProjection.apply(Projection, person, renamed)
 
       assert new_person.name == "Ben"
+    end
+  end
+
+  describe "with multiple events" do
+    test "applies changes to entity" do
+      person = %Person{name: "Joe"}
+      renamed = %Renamed{name: "Ben"}
+      aged = %Aged{amount: 3}
+
+      new_person = EntityProjection.apply(Projection, person, [renamed, aged])
+
+      assert new_person.name == "Ben" && new_person.age == 3
     end
   end
 end
