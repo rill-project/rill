6c1b57ef8ca234be266ba0be4418bd8b9922f972
Fire-Dragon-DoL <francesco.belladonna@gmail.com>
2019-01-09 19:21:14 +0100

    Change default dev config

--- config/environment/dev.exs
+++ config/environment/dev.exs
@@ -1,13 +1,8 @@
 use Mix.Config
 
 config :logger,
-  backends: [:console],
+  backends: [Scribble],
   utc_log: true,
   compile_time_purge_matching: [
     [level_lower_than: :debug]
   ]
-
-config :logger, :console,
-  format: {Rill.Logger, :format},
-  device: :standard_error,
-  metadata: :all
