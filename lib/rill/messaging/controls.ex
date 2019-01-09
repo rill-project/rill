ba6bea7a6ef28bc5c3d7271027f36b48b0278105
Fire-Dragon-DoL <francesco.belladonna@gmail.com>
2019-01-09 18:37:52 +0100

    Add controls to generate random stream names

--- lib/rill/messaging/controls.ex
+++ lib/rill/messaging/controls.ex
@@ -1,4 +1,22 @@
 defmodule Rill.Messaging.Controls do
+  defmodule Category do
+    def example(category, true) do
+      "#{category}#{UUID.uuid4()}XX"
+    end
+
+    def example(category, false) do
+      to_string(category)
+    end
+
+    def example(category) do
+      example(category, true)
+    end
+
+    def example do
+      example("", true)
+    end
+  end
+
   defmodule MessageData.SomeMessage do
     use Rill, :message
     defstruct [:some_attribute]
@@ -14,6 +32,7 @@ defmodule Rill.Messaging.Controls do
   defmacro __using__(_opts \\ []) do
     quote do
       alias unquote(__MODULE__).MessageData
+      alias unquote(__MODULE__).Category
     end
   end
 end
